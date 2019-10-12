31d0956a93fbc59cd5551ff5e8dc7e4f3676f5c7
Dean L Betty <deanlloyd64@gmail.com>
2019-10-12 00:26:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
     # remember each row should be a new instance of the Student class
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
@@ -26,6 +26,11 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT ?
+      FROM students
+      
+    SQL
   end
   
   def save
