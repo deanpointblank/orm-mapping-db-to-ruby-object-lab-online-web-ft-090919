605df50f0612936c06b68a5fe81aabb1dcee3519
Dean L Betty <deanlloyd64@gmail.com>
2019-10-12 00:27:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,10 +27,12 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-      SELECT ?
+      SELECT *
       FROM students
-      
+      WHERE name = ?
     SQL
+    
+    DB[:conn].execute(sql)
   end
   
   def save
